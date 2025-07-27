80f05582d84fd97e71709da3530956d4a93c20c7
sunho-jang <jakn4653@gmail.com>
2025-07-28 03:00:07 +0900

    Update firebase_connect.py

--- firebase_connect.py
+++ firebase_connect.py
@@ -6,5 +6,5 @@ cred = credentials.Certificate("serviceAccountKey.json")
 
 # Firebase 초기화 (Realtime Database URL은 자신의 DB URL로 바꿔줘야 해)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://discord-bot-data-xxxxx.firebaseio.com/'  # 너의 주소로 바꿔
+    'databaseURL': 'https://discord-bot-data-58877-default-rtdb.asia-southeast1.firebasedatabase.app/'  # 너의 주소로 바꿔
 })
